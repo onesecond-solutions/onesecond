e125fb34c43c325e3d41497edd5a7a803206ab83
onesecond-solutions <bylts0428@gmail.com>
2026-08-26 21:37:57 +0900

    fix(insuwork): align driving options by text rows

--- js/insuwork.js
+++ js/insuwork.js
@@ -2227,8 +2227,10 @@
     var selected = drivingStatusArray(profile.driving_status);
     var summary = selected.length ? selected.join(', ') : '선택하세요';
     var boxes = DRIVING_OPTION_GROUPS.map(function (group) {
-      return '<div class="iw-driving-row">' + group.map(function (option) {
-        return '<label class="iw-driving-option"><input type="checkbox" value="' + esc(option) + '"' + (selected.indexOf(option) >= 0 ? ' checked' : '') + ' onchange="OSInsuwork.drivingCheckChanged(this)"><span>' + esc(option) + '</span></label>';
+      var separator = group.length > 2 ? ', ' : ' / ';
+      return '<div class="iw-driving-row">' + group.map(function (option, index) {
+        var sep = index ? '<span class="iw-driving-sep">' + esc(separator) + '</span>' : '';
+        return sep + '<label class="iw-driving-option"><input type="checkbox" value="' + esc(option) + '"' + (selected.indexOf(option) >= 0 ? ' checked' : '') + ' onchange="OSInsuwork.drivingCheckChanged(this)"><span>' + esc(option) + '</span></label>';
       }).join('') + '</div>';
     }).join('');
     return '<div class="iw-driving-field">'
